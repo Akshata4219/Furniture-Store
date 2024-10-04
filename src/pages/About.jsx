ac15d5a35e8ab1811cbcf2e85fa6c085a2e5eba8
Akshata4219 <akshatanaik4219@gmail.com>
2024-10-04 17:45:30 +0530

    CommitAll

---
+++ src/pages/About.jsx
@@ -0,0 +1,30 @@
+const About = () => {
+  return (
+    <>
+      <div className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center'>
+        <h1 className='text-4xl font-bold leading-none tracking-tight sm:text-6xl'>
+          We love
+        </h1>
+        <div className='stats bg-primary shadow'>
+          <div className='stat'>
+            <div className='stat-title text-primary-content text-4xl font-bold tracking-widest'>
+              Comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className='mt-6 text-lg leading-8 max-w-2xl mx-auto'>
+        
+At Omkar Furnitures, we share your passion for comfort and believe that your home should be a sanctuary of relaxation and coziness. Our commitment to providing furniture that goes beyond mere aesthetics is reflected in our carefully curated collection of exceptionally comfortable pieces.
+<br></br>
+<br />
+Sink into the plush cushions of our sofas and lounge chairs, designed to cradle you in ultimate comfort after a long day. Our selection of recliners and accent chairs isn't just about style; it's about creating a cozy corner where you can unwind and escape the hustle and bustle of daily life.
+<br /><br />
+We understand that a good night's sleep is crucial for overall well-being. That's why our bedroom furniture is designed with comfort in mind. From sumptuously soft mattresses to luxurious bedding, we offer everything you need to create a sleep haven tailored to your preferences.
+<br /><br />
+But comfort is not limited to the living room and bedroom; it extends to every corner of your home. Explore our dining sets with ergonomically designed chairs that encourage lingering conversations around the table. Discover the joy of a home office with our ergonomic and supportive office chairs.
+      </p>
+    </>
+  );
+};
+export default About;
